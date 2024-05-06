229eb8173a9e52627de5d99b973e4f4121c76d09
Riya-SS <143711694+Riya-SS@users.noreply.github.com>
2024-05-06 20:03:56 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -13,14 +13,11 @@ connection();
 
 //middlewares
 app.use(express.json());
-app.use(cors(
-    {
-        origin: {"https://email-marketing-sequence-frontend-sbyo.vercel.app"},
-        methods: ['POST', 'GET'],
-        // allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true
-    }
-));
+app.use(cors({
+    origin: "https://email-marketing-sequence-frontend-sbyo.vercel.app",
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
 
 //routes
 app.use("/api/users", userRoutes);
